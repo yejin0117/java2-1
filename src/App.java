b31f3d20e60540e2b0b26fe39b11ca3f3c4875f2
yejin0117 <yejin5480@naver.com>
2024-03-22 12:27:10 +0900

    3221227
    수업내용

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("println");
+        System.out.println("println");
+        System.out.print("print");
+        System.out.print("print");
     }
 }
---
+++ src/chapter21.java
@@ -0,0 +1,8 @@
+// 예제2-1
+public class chapter21{
+   public static void main(String[] args) {
+    public static int sum(int n, int m){
+        s = sum(i, 10);
+    }
+   } 
+}
