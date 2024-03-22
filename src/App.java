dd9e0368ddeaaa31169264e72022ef88f89b179f
yejin0117 <yejin5480@naver.com>
2024-03-22 12:46:55 +0900

    3221246
    수업내용

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        var foo = 200;
+        var name ="google";
+        
+        System.out.println(foo+name);
         System.out.println("println");
         System.out.println("println");
         System.out.print("print");
