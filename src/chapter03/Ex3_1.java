a6eb23b5a9961c741a4a738f6b43f56c73b16e9d
yejin0117 <yejin5480@naver.com>
2024-03-29 11:25:04 +0900

    3/29 11:24 수업내용

---
+++ src/chapter03/Ex3_1.java
@@ -0,0 +1,17 @@
+package chapter03;
+
+public class Ex3_1 {
+    public static void main(String[] args) {
+        int i, sum=0;
+
+        for(i =1; i<=10; i++){
+            sum += i;
+            System.out.print(i);
+
+            if(i<=9)
+                System.out.print("+");
+            else
+                System.out.print("=");
+        }
+    }    
+}
