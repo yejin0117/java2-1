e3583e3ef27010599d147ddf3e22b4a4c29f2988
yejin0117 <yejin5480@naver.com>
2024-04-12 12:21:15 +0900

    04/12 12:21 수업내용

---
+++ src/chapter05/Ex5_02.java
@@ -0,0 +1,33 @@
+public class Ex5_02 {
+    
+    public static void main(String[] args) {
+        ColorPoint cp = new ColorPoint(5, 6, "bule");
+        cp.showColorPoint();
+    }
+}
+class Point{
+    private int x,y;
+    public Point(){
+        this.x = this.y =0;
+    }
+    public Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    public void showPoint(){
+        System.out.println("("+ x +","+ y +")");
+    }
+}
+
+class ColorPoint extends Point{
+    private String color;
+
+    public ColorPoint(int x, int y, String color){
+        super(x, y);
+        this.color = color;
+    }
+    public void showColorPoint(){
+        System.out.println(color);
+        showPoint();
+    }
+}
